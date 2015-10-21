50ffdfafb69b243764b385ff5128a7edb0e5bfc2
Zet <2etx01@gmail.com>
2015-10-21 15:44:44 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pwnable',
+    packages = find_packages(),
+    version='0.0',
+    author='Zet',
+    author_email='2etx01@gmail.com',
+    url='https://github.com/2etx01/pwnable/'
+)
