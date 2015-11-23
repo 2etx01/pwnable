3aa3fa632e9e5beef433d455aac77c1a051fc102
Zet <2etx01@gmail.com>
2015-11-23 20:20:35 +0800

    add install script

---
+++ script/setup.sh
@@ -0,0 +1,3 @@
+apt-get install zsh vim git gdb netcat-traditional netcat-openbsd nmap gcc-multilib tmux nginx bpython
+pip install git+https://github.com/Gallopsled/pwntools#egg=pwntools
+pip install git+https://github.com/2etx01/pwnable
